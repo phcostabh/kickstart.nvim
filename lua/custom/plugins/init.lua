5652aece042437503849b003e8b44b6a2f20f180
Philippe Santana Costa <philippescosta@gmail.com>
2024-03-05 14:40:08 +0400

    Trust me, it's not badly written. It's just way above your head.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,86 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+vim.opt.relativenumber = true
+vim.keymap.set('n', '<leader>fs', vim.cmd.w, { desc = '[F]ile [S]ave' })
+
+-- Enlable spell checker
+vim.opt.spell = true
+
+-- Paste replace visual selection without copying it.
+vim.keymap.set('v', 'p', '"_dP')
+
+-- Remap ";" to ":"
+vim.keymap.set('n', ';', ':')
+
+-- Move text up and down
+vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('x', '<A-j>', ":move '>+1<CR>gv-gv")
+vim.keymap.set('x', '<A-k>', ":move '<-2<CR>gv-gv")
+
+-- Add syntax highlight to .env files
+vim.api.nvim_create_autocmd('BufRead', {
+  callback = function()
+    vim.filetype.add {
+      filename = {
+        ['.env'] = 'sh',
+      },
+    }
+  end,
+})
+
+return {
+  {
+    'vim-test/vim-test',
+    config = function()
+      vim.keymap.set('n', '<Leader>tn', ':TestNearest<CR>')
+      vim.keymap.set('n', '<Leader>tf', ':TestFile<CR>')
+      vim.keymap.set('n', '<Leader>ts', ':TestSuite<CR>')
+      vim.keymap.set('n', '<Leader>tl', ':TestLast<CR>')
+      vim.keymap.set('n', '<Leader>tv', ':TestVisit<CR>')
+    end,
+  },
+
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      require('rose-pine').setup {
+        styles = {
+          italic = false,
+          bold = false,
+          transparency = true,
+        },
+        enable = {
+          migrations = true,
+        },
+      }
+
+      vim.cmd 'colorscheme rose-pine'
+    end,
+  },
+
+  {
+    'github/copilot.vim',
+    setup = function()
+      vim.g.copilot_assume_mapped = true
+    end,
+  },
+
+  -- {
+  --   'jackMort/ChatGPT.nvim',
+  --   event = 'VeryLazy',
+  --   config = function()
+  --     require('chatgpt').setup {
+  --       api_key_cmd = "op read 'op://Private/ChatGPT.nvim/api key' --no-newline",
+  --     }
+  --   end,
+  --   dependencies = {
+  --     'MunifTanjim/nui.nvim',
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-telescope/telescope.nvim',
+  --   },
+  -- },
+}
